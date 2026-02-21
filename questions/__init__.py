092b1f91bc37fbfe9c6eb7752f3eabce768b03a4
FedorMilovanov <viktorcoy2012@gmail.com>
2026-02-22 02:32:30 +0300

    Create __init__.py

---
+++ questions/__init__.py
@@ -0,0 +1,19 @@
+from .chapter1 import (
+    easy_questions, easy_questions_v17_25,
+    medium_questions, medium_questions_v17_25,
+    hard_questions, hard_questions_v17_25,
+    nero_questions, geography_questions,
+    practical_ch1_questions, practical_v17_25_questions,
+    linguistics_ch1_questions, linguistics_ch1_questions_2, linguistics_v17_25_questions,
+    all_chapter1_questions,
+)
+
+__all__ = [
+    "easy_questions", "easy_questions_v17_25",
+    "medium_questions", "medium_questions_v17_25",
+    "hard_questions", "hard_questions_v17_25",
+    "nero_questions", "geography_questions",
+    "practical_ch1_questions", "practical_v17_25_questions",
+    "linguistics_ch1_questions", "linguistics_ch1_questions_2", "linguistics_v17_25_questions",
+    "all_chapter1_questions",
+]
