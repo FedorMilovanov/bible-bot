731205f3d6ef8a9a978f2c5e100a67432654cf11
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-13 23:41:03 +0300

    Add reviewed sources for 1 Peter 2:11-25

---
+++ questions/chapter2/sources_11_25.py
@@ -0,0 +1,51 @@
+"""Reviewed sources for 1 Peter 2:11-25."""
+
+SOURCE_CATALOG = {
+    "gty_1p2_18_21": {
+        "title": "John MacArthur, Why Does the Christian's Work Ethic Matter?",
+        "url": "https://shop.gty.org/library/bibleqnas-library/QA0202/why-does-the-christians-work-ethic-matter",
+        "kind": "conservative_application",
+    },
+    "gty_1p2_21_23": {
+        "title": "John MacArthur, The Suffering Jesus: An Example for Every Christian",
+        "url": "https://www.gty.org/resources/study-guides/chapters/60-29/the-suffering-jesus-an-example-for-every-christian",
+        "kind": "conservative_exposition",
+    },
+    "gty_1p2_24_25": {
+        "title": "John MacArthur, The Suffering Jesus: Our Substitute and Shepherd",
+        "url": "https://www.gty.org/resources/study-guides/chapters/60-30/the-suffering-jesus-our-substitute-and-shepherd",
+        "kind": "conservative_exposition",
+    },
+    "gty_civil_disobedience": {
+        "title": "John MacArthur, Can Christians Participate in Civil Disobedience?",
+        "url": "https://www.gty.org/articles/A120/can-christians-participate-in-civil-disobedience",
+        "kind": "conservative_application",
+    },
+    "tmsj_felix_penal_substitution": {
+        "title": "Paul W. Felix Sr., Penal Substitution in the New Testament: A Focused Look at First Peter, TMSJ 20.2 (2009)",
+        "url": "https://tms.edu/educational-resources/journal/archive/",
+        "kind": "conservative_scholarship",
+    },
+    "tms_doctrinal_statement": {
+        "title": "The Master's Seminary Doctrinal Statement",
+        "url": "https://tms.edu/doctrinal-statement/",
+        "kind": "project_theology_control",
+    },
+    "cambridge_missing_masters": {
+        "title": "Jason Maston, The Missing Masters of 1 Peter, New Testament Studies 71.2 (2025)",
+        "url": "https://www.cambridge.org/core/journals/new-testament-studies/article/abs/missing-masters-of-1-peter/8CF9B1DEEFEFBA2537B69A8930A24264",
+        "kind": "scholarship",
+    },
+    "cambridge_following_footsteps": {
+        "title": "Following in the Footsteps: Exemplarity, Ethnicity and Ethics in 1 Peter, New Testament Studies",
+        "url": "https://www.cambridge.org/core/journals/new-testament-studies/article/following-in-the-footsteps-exemplarity-ethnicity-and-ethics-in-1-peter/F19BD804EBB5FF4CD0259B3C75C0CE8A",
+        "kind": "scholarship",
+    },
+    "hts_ancient_slavery": {
+        "title": "Hendrik Goede, Constructing ancient slavery as socio-historic context of the New Testament",
+        "url": "https://hts.org.za/index.php/hts/article/view/1297",
+        "kind": "scholarship",
+    },
+}
+
+__all__ = ["SOURCE_CATALOG"]
