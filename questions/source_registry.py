33eaffd4a7b212a777ea77be76a7488d407758c3
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-14 00:04:13 +0300

    Register chapter 2 disputed sources

--- questions/source_registry.py
+++ questions/source_registry.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from .chapter2.sources import SOURCE_CATALOG as CHAPTER2_SOURCE_CATALOG
 from .chapter2.sources_11_25 import SOURCE_CATALOG as CHAPTER2_11_25_SOURCE_CATALOG
+from .chapter2.sources_disputed import SOURCE_CATALOG as CHAPTER2_DISPUTED_SOURCE_CATALOG
 from .content_truth import SOURCE_CATALOG as LEGACY_SOURCE_CATALOG
 
 
@@ -21,6 +22,7 @@ SOURCE_CATALOG = _merge_catalogs(
     LEGACY_SOURCE_CATALOG,
     CHAPTER2_SOURCE_CATALOG,
     CHAPTER2_11_25_SOURCE_CATALOG,
+    CHAPTER2_DISPUTED_SOURCE_CATALOG,
 )
 
 
