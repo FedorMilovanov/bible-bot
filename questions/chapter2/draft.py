80c678bfc466ea6287034dda7c06d69fca1d26cd
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-14 00:05:34 +0300

    Add reviewed Hosea and theology modules to chapter 2 draft

--- questions/chapter2/draft.py
+++ questions/chapter2/draft.py
@@ -4,6 +4,7 @@ from .greek import GREEK_2_1_10
 from .greek_11_17 import GREEK_2_11_17
 from .greek_18_25 import GREEK_2_18_25
 from .ot_exodus19 import OT_EXODUS19_2_9
+from .ot_hosea_2_10 import OT_HOSEA_2_10
 from .ot_isaiah53 import OT_ISAIAH53_2_21_25
 from .ot_psalm34 import OT_PSALM34_2_3
 from .ot_stone import OT_STONE_2_6_8
@@ -14,11 +15,13 @@ from .text_16_17 import TEXT_2_16_17
 from .text_18_20 import TEXT_2_18_20
 from .text_21_25 import TEXT_2_21_25
 from .theology_21_25 import THEOLOGY_2_21_25
+from .theology_growth import THEOLOGY_GROWTH_2_1_3
+from .theology_house import THEOLOGY_HOUSE_2_4_5
 
 TEXT_2_DRAFT = TEXT_2_1_10 + TEXT_2_11_12 + TEXT_2_13 + TEXT_2_16_17 + TEXT_2_18_20 + TEXT_2_21_25
 GREEK_2_DRAFT = GREEK_2_1_10 + GREEK_2_11_17 + GREEK_2_18_25
-INTERTEXT_2_DRAFT = OT_PSALM34_2_3 + OT_STONE_2_6_8 + OT_EXODUS19_2_9 + OT_ISAIAH53_2_21_25
-THEOLOGY_2_DRAFT = THEOLOGY_2_21_25
+INTERTEXT_2_DRAFT = OT_PSALM34_2_3 + OT_STONE_2_6_8 + OT_EXODUS19_2_9 + OT_HOSEA_2_10 + OT_ISAIAH53_2_21_25
+THEOLOGY_2_DRAFT = THEOLOGY_GROWTH_2_1_3 + THEOLOGY_HOUSE_2_4_5 + THEOLOGY_2_21_25
 
 CHAPTER2_DRAFT_QUESTIONS = TEXT_2_DRAFT + GREEK_2_DRAFT + INTERTEXT_2_DRAFT + THEOLOGY_2_DRAFT
 
