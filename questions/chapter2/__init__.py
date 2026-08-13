15859e065377c0c34153b86e314f5a4e1950c830
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-13 23:38:25 +0300

    Assemble chapter 2 intertext draft

--- questions/chapter2/__init__.py
+++ questions/chapter2/__init__.py
@@ -4,12 +4,21 @@ The chapter is not production-complete until 2:1-25 passes the coverage roadmap.
 """
 
 from .greek import GREEK_2_1_10
+from .ot_exodus19 import OT_EXODUS19_2_9
+from .ot_psalm34 import OT_PSALM34_2_3
+from .ot_stone import OT_STONE_2_6_8
 from .text import TEXT_2_1_10
 
-CHAPTER2_DRAFT_QUESTIONS = TEXT_2_1_10 + GREEK_2_1_10
+INTERTEXT_2_1_10 = OT_PSALM34_2_3 + OT_STONE_2_6_8 + OT_EXODUS19_2_9
+
+CHAPTER2_DRAFT_QUESTIONS = TEXT_2_1_10 + GREEK_2_1_10 + INTERTEXT_2_1_10
 
 __all__ = [
     "CHAPTER2_DRAFT_QUESTIONS",
     "GREEK_2_1_10",
+    "INTERTEXT_2_1_10",
+    "OT_EXODUS19_2_9",
+    "OT_PSALM34_2_3",
+    "OT_STONE_2_6_8",
     "TEXT_2_1_10",
 ]
